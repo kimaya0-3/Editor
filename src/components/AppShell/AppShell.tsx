39d03185aaae5c3effc6ec47de854b68ec239301
kimaya0-3 <kimaya.padmashali@siemens.com>
2026-07-24 09:43:35 +0200

    side panel

--- src/components/AppShell/AppShell.tsx
+++ src/components/AppShell/AppShell.tsx
@@ -4,6 +4,7 @@ import { useCallback }       from 'react'
 import { ReactFlowProvider } from '@xyflow/react'
 import { Toolbar }           from '../Toolbar/Toolbar'
 import { EditorPanel }       from '../Editor/EditorPanel'
+import { SidePanel }         from '../SidePanel/SidePanel'
 import { Canvas }            from '../Canvas/Canvas'
 import { LegendTooltip }     from '../Canvas/LegendTooltip'
 import { useProjectStore }   from '../../store/projectStore'
@@ -52,6 +53,8 @@ export const AppShell = () => {
           }}>
             <Canvas />
           </div>
+
+          <SidePanel />
 
         </div>
 
